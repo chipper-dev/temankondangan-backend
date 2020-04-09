1222bbe4500dfb271ae1f361680bd8d2ada884cb
Yuliawan Rizka S <yuliawanrizka.syafaat@mitrais.com>
2020-04-09 09:08:29 +0700

    separate users and profiles.

--- sql/1.sql
+++ sql/1.sql
@@ -2,6 +2,16 @@ CREATE TABLE public.users (
 	id serial NOT NULL,
 	email varchar NOT NULL,
 	password_hashed varchar NOT NULL,
+	created_by varchar NOT NULL,
+	created_date date NOT NULL,
+	modified_by varchar NOT NULL,
+	modified_date date NOT NULL,
+	CONSTRAINT users_pkey PRIMARY KEY (id)
+);
+
+CREATE table public.profiles (
+	id serial NOT NULL,
+	user_pk int4 NOT NULL REFERENCES users(id),
 	fullname varchar NOT NULL,
 	dob date NOT NULL,
 	gender varchar(1) NOT NULL,
@@ -9,5 +19,5 @@ CREATE TABLE public.users (
 	city varchar NULL,
 	about_me varchar(200) NULL,
 	interest varchar(200) NULL,
-	CONSTRAINT users_pkey PRIMARY KEY (id)
+	CONSTRAINT profiles_pkey PRIMARY KEY (id)
 );
