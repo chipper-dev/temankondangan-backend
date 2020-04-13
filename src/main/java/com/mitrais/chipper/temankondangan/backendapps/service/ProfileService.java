9c0d30ecdb6dcd778ea8f57731c026388b7c6b2a
Doni Kuncoro Rizky <donikrizky@gmail.com>
2020-04-13 09:49:33 +0700

    Revert "-add Users & Profile model"
    This reverts commit 80dedd096c83ac2367af839c7220596f6c2d9009.

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/service/ProfileService.java
+++
@@ -1,9 +0,0 @@
-package com.mitrais.chipper.temankondangan.backendapps.service;
-
-import com.mitrais.chipper.temankondangan.backendapps.model.Profile;
-
-public interface ProfileService {
-
-	public Profile getProfile(Long userId);
-
-}
