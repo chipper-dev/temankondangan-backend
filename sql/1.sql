c163c4cd496794738744d06fb3a3b75962667c80
Yuliawan Rizka S <yuliawanrizka.syafaat@mitrais.com>
2020-04-13 13:23:57 +0700

    fix SQL

--- sql/1.sql
+++ sql/1.sql
@@ -9,16 +9,20 @@ CREATE TABLE public.users (
 	CONSTRAINT users_pkey PRIMARY KEY (id)
 );
 
-CREATE table public.profiles (
+CREATE table public.profile (
 	id serial NOT NULL,
 	user_pk int4 NOT NULL REFERENCES users(id),
-	fullname varchar NOT NULL,
+	full_name varchar NOT NULL,
 	dob date NOT NULL,
 	gender varchar(1) NOT NULL,
 	photo_profile bytea NULL,
 	city varchar NULL,
 	about_me varchar(200) NULL,
 	interest varchar(200) NULL,
+	created_by varchar NOT NULL,
+    created_date date NOT NULL,
+    modified_by varchar NOT NULL,
+    modified_date date NOT NULL,
 	CONSTRAINT profiles_pkey PRIMARY KEY (id)
 );
 
