8274fd820bbebc20b879bb583f1cd77db0cd96f0
Yuliawan Rizka S <yuliawanrizka.syafaat@mitrais.com>
2020-04-13 13:23:37 +0700

    Add spring security

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/BackendAppsApplication.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/BackendAppsApplication.java
@@ -1,13 +1,25 @@
 package com.mitrais.chipper.temankondangan.backendapps;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
 public class BackendAppsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendAppsApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
