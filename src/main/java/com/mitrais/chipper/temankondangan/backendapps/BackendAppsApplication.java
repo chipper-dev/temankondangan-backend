c665dd6d56ea1cddf0e8f7f1f1bfafbdc8368dfd
Enjang_AB241 <EnjangAkbar.Binaji@mitrais.com>
2020-04-14 13:46:17 +0800

    Enabling AppProperties

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/BackendAppsApplication.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/BackendAppsApplication.java
@@ -1,9 +1,11 @@
 package com.mitrais.chipper.temankondangan.backendapps;
 
+import com.mitrais.chipper.temankondangan.backendapps.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,7 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@Configuration
+@EnableConfigurationProperties(AppProperties.class)
 public class BackendAppsApplication {
 
 	public static void main(String[] args) {
