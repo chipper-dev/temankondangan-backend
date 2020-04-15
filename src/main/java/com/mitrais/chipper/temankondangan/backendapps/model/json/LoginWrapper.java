6722cdcf40251b229ff65a810a7020fa7d81b788
Yuliawan Rizka S <yuliawanrizka.syafaat@mitrais.com>
2020-04-15 15:29:58 +0700

    change format and add wrapper

---
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/model/json/LoginWrapper.java
@@ -0,0 +1,15 @@
+package com.mitrais.chipper.temankondangan.backendapps.model.json;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class LoginWrapper {
+    private String email;
+    private String password;
+}
