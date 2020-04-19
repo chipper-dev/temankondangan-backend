ce0bff1f7697e9fe6c3a1811c43ad4b2aedb9986
Doni Kuncoro Rizky <donikrizky@gmail.com>
2020-04-20 01:18:36 +0700

    -bug fix for change password API -bug fix for create password API

--- src/test/java/com/mitrais/chipper/temankondangan/backendapps/service/UserServiceTest.java
+++ src/test/java/com/mitrais/chipper/temankondangan/backendapps/service/UserServiceTest.java
@@ -1,11 +1,11 @@
 package com.mitrais.chipper.temankondangan.backendapps.service;
 
-import com.mitrais.chipper.temankondangan.backendapps.model.User;
-import com.mitrais.chipper.temankondangan.backendapps.model.json.UserChangePasswordWrapper;
-import com.mitrais.chipper.temankondangan.backendapps.model.json.UserCreatePasswordWrapper;
-import com.mitrais.chipper.temankondangan.backendapps.repository.UserRepository;
-import com.mitrais.chipper.temankondangan.backendapps.security.TokenProvider;
-import com.mitrais.chipper.temankondangan.backendapps.service.impl.UserServiceImpl;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -14,13 +14,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import com.mitrais.chipper.temankondangan.backendapps.model.User;
+import com.mitrais.chipper.temankondangan.backendapps.model.json.UserChangePasswordWrapper;
+import com.mitrais.chipper.temankondangan.backendapps.model.json.UserCreatePasswordWrapper;
+import com.mitrais.chipper.temankondangan.backendapps.repository.UserRepository;
+import com.mitrais.chipper.temankondangan.backendapps.security.TokenProvider;
+import com.mitrais.chipper.temankondangan.backendapps.service.impl.UserServiceImpl;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -40,38 +41,76 @@ public class UserServiceTest {
 
 	@BeforeEach
 	public void init() {
-		User user = new User(1L, "test@email.com", passwordEncoder.encode("123"), "test user", LocalDateTime.now(), "test user",
-				LocalDateTime.now(), null, null);
+		User user = new User(1L, "test@email.com", passwordEncoder.encode("12345_"), "test user", LocalDateTime.now(),
+				"test user", LocalDateTime.now(), null, null);
 		Optional<User> userOptional = Optional.of(user);
 		Mockito.when(userRepository.findById(Mockito.any(Long.class))).thenReturn(userOptional);
 		Mockito.when(tokenProvider.getUserIdFromToken(Mockito.anyString())).thenReturn(1L);
 	}
 
+//	Testing for Change Password API
 	@Test
 	public void ChangePasswordTest() {
-		UserChangePasswordWrapper wrapper = new UserChangePasswordWrapper("123", "12345", "12345");
+		UserChangePasswordWrapper wrapper = new UserChangePasswordWrapper("12345_", "123456_", "123456_");
 		boolean result = userService.changePassword(wrapper, "dummy token");
 		assertTrue(result);
 	}
 
 	@Test
-	public void ChangePasswordDifferentOldPassword() {
-		UserChangePasswordWrapper wrapper = new UserChangePasswordWrapper("123Q", "12345", "12345");
-		boolean result = userService.changePassword(wrapper, "dummy token");
-		assertFalse(result);
+	public void ShouldThrowResponseStatusException_WhenPasswordEmpty() {
+		UserChangePasswordWrapper wrapper = new UserChangePasswordWrapper("12345_", "", "");
+		assertThatThrownBy(() -> userService.changePassword(wrapper, "dummy token"))
+				.isInstanceOf(ResponseStatusException.class);
 	}
 
+	@Test
+	public void ShouldThrowResponseStatusException_WhenOldPasswordNotMatch() {
+		UserChangePasswordWrapper wrapper = new UserChangePasswordWrapper("12345_@", "123456_", "123456_");
+		assertThatThrownBy(() -> userService.changePassword(wrapper, "dummy token"))
+				.isInstanceOf(ResponseStatusException.class);
+	}
+
+	@Test
+	public void ShouldThrowResponseStatusException_WhenOldPasswordSameAsNewPassword() {
+		UserChangePasswordWrapper wrapper = new UserChangePasswordWrapper("12345_", "12345_", "12345_");
+		assertThatThrownBy(() -> userService.changePassword(wrapper, "dummy token"))
+				.isInstanceOf(ResponseStatusException.class);
+	}
+
+	@Test
+	public void ShouldThrowResponseStatusException_WhenNewPasswordDoNotMatchWithConfirmedPassword() {
+		UserChangePasswordWrapper wrapper = new UserChangePasswordWrapper("12345_", "123456_", "123457_");
+		assertThatThrownBy(() -> userService.changePassword(wrapper, "dummy token"))
+				.isInstanceOf(ResponseStatusException.class);
+	}
+
+	@Test
+	public void ShouldThrowResponseStatusException_WhenNewPasswordLengthNotRight() {
+		UserChangePasswordWrapper wrapper = new UserChangePasswordWrapper("12345_", "12_", "12_");
+		assertThatThrownBy(() -> userService.changePassword(wrapper, "dummy token"))
+				.isInstanceOf(ResponseStatusException.class);
+	}
+
+	@Test
+	public void ShouldThrowResponseStatusException_WhenNewPasswordDoNotHaveSpecialChar() {
+		UserChangePasswordWrapper wrapper = new UserChangePasswordWrapper("12345_", "123456", "123456");
+		assertThatThrownBy(() -> userService.changePassword(wrapper, "dummy token"))
+				.isInstanceOf(ResponseStatusException.class);
+	}
+
+	@Test
+	public void ShouldThrowResponseStatusException_WhenNewPasswordDoNotHaveDigit() {
+		UserChangePasswordWrapper wrapper = new UserChangePasswordWrapper("12345_", "ABCDEF_", "ABCDEF_");
+		assertThatThrownBy(() -> userService.changePassword(wrapper, "dummy token"))
+				.isInstanceOf(ResponseStatusException.class);
+	}
+
+	// Testing for Create Password API
 	@Test
 	public void CreatePasswordTest() {
-		UserCreatePasswordWrapper wrapper = new UserCreatePasswordWrapper("12345", "12345");
+		UserCreatePasswordWrapper wrapper = new UserCreatePasswordWrapper("12345_", "12345_");
 		boolean result = userService.createPassword(wrapper, "dummy token");
 		assertTrue(result);
 	}
 
-	@Test
-	public void CreatePasswordDifferentOldPassword() {
-		UserCreatePasswordWrapper wrapper = new UserCreatePasswordWrapper("12345", "12345Q");
-		boolean result = userService.createPassword(wrapper, "dummy token");
-		assertFalse(result);
-	}
 }
