82cd218185a4f148a280fc1015b5da3741f4067a
akbarb24 <akbar.binaji@gmail.com>
2020-04-23 15:19:29 +0800

    add FirebaseConfig

---
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/config/FirebaseConfig.java
@@ -0,0 +1,36 @@
+package com.mitrais.chipper.temankondangan.backendapps.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.mitrais.chipper.temankondangan.backendapps.BackendAppsApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+    private final static Logger logger = LoggerFactory.getLogger(FirebaseConfig.class);
+
+    @Primary
+    @Bean
+    public void firebaseInit() throws IOException, FirebaseAuthException {
+        InputStream serviceAccount = BackendAppsApplication.class.getResourceAsStream("/temankondangan-92149-firebase-adminsdk-9pnh2-6d63144122.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://temankondangan-92149.firebaseio.com")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+        logger.info("Firebase app: {} initialized", FirebaseApp.getInstance().getName());
+    }
+}
