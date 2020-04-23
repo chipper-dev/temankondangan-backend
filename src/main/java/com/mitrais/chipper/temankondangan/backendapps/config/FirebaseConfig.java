d3842b8cf57157f5b480a557f38bd226b701d0fc
akbarb24 <akbar.binaji@gmail.com>
2020-04-23 15:25:48 +0800

    change resource file

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/config/FirebaseConfig.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/config/FirebaseConfig.java
@@ -3,7 +3,6 @@ package com.mitrais.chipper.temankondangan.backendapps.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.mitrais.chipper.temankondangan.backendapps.BackendAppsApplication;
 import org.slf4j.Logger;
@@ -22,7 +21,7 @@ public class FirebaseConfig {
     @Primary
     @Bean
     public void firebaseInit() throws IOException, FirebaseAuthException {
-        InputStream serviceAccount = BackendAppsApplication.class.getResourceAsStream("/temankondangan-92149-firebase-adminsdk-9pnh2-6d63144122.json");
+        InputStream serviceAccount = BackendAppsApplication.class.getResourceAsStream("/serviceAccountKey.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
