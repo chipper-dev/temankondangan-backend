a8d4643f58a6a9a025ddd77e1fdc1f36a0836245
akbarb24 <akbar.binaji@gmail.com>
2020-04-24 16:33:14 +0800

    remove unnecessary config

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/config/AppProperties.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/config/AppProperties.java
@@ -8,7 +8,6 @@ import java.util.List;
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
     private final Auth auth = new Auth();
-    private final OAuth2 oauth2 = new OAuth2();
 
     public static class Auth {
         private String tokenSecret;
@@ -31,24 +30,7 @@ public class AppProperties {
         }
     }
 
-    public static final class OAuth2 {
-        private List<String> authorizedRedirectUris = new ArrayList<>();
-
-        public List<String> getAuthorizedRedirectUris() {
-            return authorizedRedirectUris;
-        }
-
-        public OAuth2 authorizedRedirectUris(List<String> authorizedRedirectUris) {
-            this.authorizedRedirectUris = authorizedRedirectUris;
-            return this;
-        }
-    }
-
     public Auth getAuth() {
         return auth;
-    }
-
-    public OAuth2 getOauth2() {
-        return oauth2;
     }
 }
