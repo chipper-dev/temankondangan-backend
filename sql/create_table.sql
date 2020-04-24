11847bf166aa3108e6216da0a825f3d549c9d20e
akbarb24 <akbar.binaji@gmail.com>
2020-04-24 16:27:17 +0800

    update script

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -36,34 +36,6 @@ CREATE TABLE public.config (
 	modified_date timestamp NOT NULL
 );
 
-ALTER TABLE public.users
-RENAME COLUMN modified_by TO last_modified_by;
-
-ALTER TABLE public.users
-RENAME COLUMN modified_date TO last_modified_date;
-
-ALTER TABLE public.profile
-RENAME COLUMN modified_by TO last_modified_by;
-
-ALTER TABLE public.profile
-RENAME COLUMN modified_date TO last_modified_date;
-
-ALTER TABLE public.config
-RENAME COLUMN modified_by TO last_modified_by;
-
-ALTER TABLE public.config
-RENAME COLUMN modified_date TO last_modified_date;
-
-ALTER TABLE users
-ADD COLUMN data_state VARCHAR NOT NULL DEFAULT 'ACTIVE';
-
-ALTER TABLE users 
-ALTER COLUMN data_state 
-DROP DEFAULT;
-
-ALTER TABLE public.users
-ADD COLUMN logout timestamp NULL;
-
 CREATE TABLE public.event
 (
     event_id int8 PRIMARY KEY,
@@ -79,6 +51,3 @@ CREATE TABLE public.event
 	last_modified_by varchar NOT NULL,
 	last_modified_date timestamp NOT NULL
 )
-
-ALTER TABLE public.users
-RENAME COLUMN provider_id TO uid;
