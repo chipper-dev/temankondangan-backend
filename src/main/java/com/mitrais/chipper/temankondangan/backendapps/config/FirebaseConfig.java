d0480aa797aa8cb33a3b2e398c0bdecdaaff7d3c
akbarb24 <akbar.binaji@gmail.com>
2020-04-27 10:28:34 +0800

    change FirebaseConfig into env variable

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/config/FirebaseConfig.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/config/FirebaseConfig.java
@@ -3,29 +3,30 @@ package com.mitrais.chipper.temankondangan.backendapps.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.mitrais.chipper.temankondangan.backendapps.BackendAppsApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
     private final static Logger logger = LoggerFactory.getLogger(FirebaseConfig.class);
 
+    @Value("${app.firebase.databaseUrl}")
+    String firebaseDatabaseUrl;
+
+
     @Primary
     @Bean
-    public void firebaseInit() throws IOException, FirebaseAuthException {
-        InputStream serviceAccount = BackendAppsApplication.class.getResourceAsStream("/serviceAccountKey.json");
+    public void firebaseInit() throws IOException {
 
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://temankondangan-92149.firebaseio.com")
+                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .setDatabaseUrl(firebaseDatabaseUrl)
                 .build();
 
         if (FirebaseApp.getApps().isEmpty()) {
