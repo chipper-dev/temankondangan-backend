5b419a2c7e4426a85086c6faf579bc2a25edc834
akbarb24 <akbar.binaji@gmail.com>
2020-04-27 16:16:41 +0800

    create ImageFileService

---
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/service/impl/ImageFileServiceImpl.java
@@ -0,0 +1,22 @@
+package com.mitrais.chipper.temankondangan.backendapps.service.impl;
+
+import com.mitrais.chipper.temankondangan.backendapps.model.Profile;
+import com.mitrais.chipper.temankondangan.backendapps.repository.ProfileRepository;
+import com.mitrais.chipper.temankondangan.backendapps.service.ImageFileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+
+@Service
+public class ImageFileServiceImpl implements ImageFileService {
+    @Autowired
+    ProfileRepository profileRepository;
+
+    public Profile getImage(String profileIdStr) throws FileNotFoundException {
+        Long profileId = Long.parseLong(profileIdStr);
+
+        return profileRepository.findById(profileId)
+                .orElseThrow(() -> new FileNotFoundException("File not found with id " + profileId));
+    }
+}
