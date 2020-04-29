097732e44eb50e95b1f3bee7468dc0d65ce4b063
akbarb24 <akbar.binaji@gmail.com>
2020-04-29 14:16:18 +0800

    migrate to junit5

--- src/test/java/com/mitrais/chipper/temankondangan/backendapps/service/EmailServiceTest.java
+++ src/test/java/com/mitrais/chipper/temankondangan/backendapps/service/EmailServiceTest.java
@@ -1,14 +1,13 @@
 package com.mitrais.chipper.temankondangan.backendapps.service;
 
 import com.mitrais.chipper.temankondangan.backendapps.service.impl.EmailServiceImpl;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class EmailServiceTest {
     @Autowired
     EmailServiceImpl emailService;
