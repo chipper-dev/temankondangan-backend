25259720a4e714728168b2654e6a00711d0e2799
akbarb24 <akbar.binaji@gmail.com>
2020-04-29 13:01:26 +0800

    update by adding findByEmail n findByCode method

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/repository/VerificationCodeRepository.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/repository/VerificationCodeRepository.java
@@ -4,6 +4,11 @@ import com.mitrais.chipper.temankondangan.backendapps.model.VerificationCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Long> {
+    Optional<VerificationCode> findByCode(String code);
+    List<VerificationCode> findByEmail(String email);
 }
