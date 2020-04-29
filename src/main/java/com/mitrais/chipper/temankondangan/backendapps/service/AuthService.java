fe72543fdff0ae686c24bf57b0aa308e079d6259
akbarb24 <akbar.binaji@gmail.com>
2020-04-29 13:02:35 +0800

    update by adding reset password service method handler

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/service/AuthService.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/service/AuthService.java
@@ -2,10 +2,12 @@ package com.mitrais.chipper.temankondangan.backendapps.service;
 
 import com.mitrais.chipper.temankondangan.backendapps.model.User;
 import com.mitrais.chipper.temankondangan.backendapps.model.json.RegisterUserWrapper;
+import com.mitrais.chipper.temankondangan.backendapps.model.json.ResetPasswordWrapper;
 
 public interface AuthService {
     User save(RegisterUserWrapper register);
     boolean login(String email, String password);
     boolean logout(Long userId);
     void forgotPassword(String email);
+    void resetPassword(ResetPasswordWrapper wrapper);
 }
--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/service/impl/AuthServiceImpl.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/service/impl/AuthServiceImpl.java
@@ -1,17 +1,23 @@
 package com.mitrais.chipper.temankondangan.backendapps.service.impl;
 
+import com.mitrais.chipper.temankondangan.backendapps.config.FirebaseConfig;
 import com.mitrais.chipper.temankondangan.backendapps.model.Profile;
 import com.mitrais.chipper.temankondangan.backendapps.model.VerificationCode;
 import com.mitrais.chipper.temankondangan.backendapps.model.User;
 import com.mitrais.chipper.temankondangan.backendapps.model.en.AuthProvider;
 import com.mitrais.chipper.temankondangan.backendapps.model.en.DataState;
 import com.mitrais.chipper.temankondangan.backendapps.model.json.RegisterUserWrapper;
+import com.mitrais.chipper.temankondangan.backendapps.model.json.ResetPasswordWrapper;
 import com.mitrais.chipper.temankondangan.backendapps.repository.ProfileRepository;
-import com.mitrais.chipper.temankondangan.backendapps.repository.TempPasswordRepository;
+import com.mitrais.chipper.temankondangan.backendapps.repository.VerificationCodeRepository;
 import com.mitrais.chipper.temankondangan.backendapps.repository.UserRepository;
 import com.mitrais.chipper.temankondangan.backendapps.service.AuthService;
 import com.mitrais.chipper.temankondangan.backendapps.service.EmailService;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,35 +25,37 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.transaction.Transactional;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.format.ResolverStyle;
-import java.util.Date;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Random;
+import java.util.*;
 import java.util.regex.Pattern;
 
 @Service
 public class AuthServiceImpl implements AuthService {
+    private final static Logger logger = LoggerFactory.getLogger(AuthServiceImpl.class);
+
+    @Value("${app.verificationExpirationMsec}")
+    Long expiration;
 
     PasswordEncoder passwordEncoder;
     UserRepository userRepository;
     ProfileRepository profileRepository;
-    TempPasswordRepository passwordRepository;
+    VerificationCodeRepository verificationRepository;
     EmailService emailService;
     SimpleMailMessage template;
 
     @Autowired
-    public AuthServiceImpl(PasswordEncoder passwordEncoder, UserRepository userRepository, ProfileRepository profileRepository, EmailService emailService, SimpleMailMessage template, TempPasswordRepository tempPasswordRepository) {
+    public AuthServiceImpl(PasswordEncoder passwordEncoder, UserRepository userRepository, ProfileRepository profileRepository, EmailService emailService, SimpleMailMessage template, VerificationCodeRepository verificationCodeRepository) {
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
         this.profileRepository = profileRepository;
         this.emailService = emailService;
         this.template = template;
-        this.passwordRepository = tempPasswordRepository;
+        this.verificationRepository = verificationCodeRepository;
     }
 
     @Override
@@ -152,10 +160,38 @@ public class AuthServiceImpl implements AuthService {
     public void forgotPassword(String email) {
         Integer code = generateRandomCode(6);
         User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("Your email is not registered. Please try again"));
+                .orElseThrow(() -> new RuntimeException("Error: Your email is not registered. Please try again"));
 
         saveCode(user.getEmail(), code);
         sendEmailJob(user.getEmail(), code);
+    }
+
+    @Override
+    public void resetPassword(ResetPasswordWrapper wrapper) {
+        resetPasswordValidation(wrapper);
+
+        VerificationCode verificationCode = verificationRepository.findByCode(wrapper.getVerificationCode())
+                .orElseThrow(() -> new RuntimeException("Error: Please input correct verification code from your email"));
+
+        // check verification code is expired?
+        if (isCodeValid(verificationCode.getCreatedAt())) {
+            User user = userRepository.findByEmail(verificationCode.getEmail())
+                    .orElseThrow(() -> new RuntimeException("Error: User not Found"));
+
+            // check the new password can't be same with old password
+            if (passwordEncoder.matches(wrapper.getNewPassword(), user.getPasswordHashed()))
+                throw new RuntimeException("Error: New password must be different from current password");
+
+            // Update password
+            user.setPasswordHashed(passwordEncoder.encode(wrapper.getNewPassword()));
+            userRepository.save(user);
+
+            // Delete verification code
+            deleteCode(user.getEmail());
+        } else {
+            //Expired
+            throw new RuntimeException("Error: Please input correct verification code from your email");
+        }
     }
 
     private Integer generateRandomCode(Integer n) {
@@ -170,12 +206,53 @@ public class AuthServiceImpl implements AuthService {
                 .createdAt(LocalDateTime.now())
                 .build();
 
-        passwordRepository.save(temp);
+        verificationRepository.save(temp);
+    }
+
+    private void deleteCode(String email) {
+        List<VerificationCode> list = verificationRepository.findByEmail(email);
+        if(!list.isEmpty()) {
+            list.forEach(verificationCode -> {
+                logger.info("Delete code: {}", verificationCode.getCode());
+                verificationRepository.delete(verificationCode);
+            });
+        }
     }
 
     private void sendEmailJob(String email, Integer code) {
         String subject = "Teman Kondangan - Reset Password";
         String body = String.format(Objects.requireNonNull(template.getText()), email, code);
         emailService.sendMessage(email, subject, body);
+    }
+
+    private boolean isCodeValid(LocalDateTime createdAt) {
+        Duration duration = Duration.between(createdAt, LocalDateTime.now());
+
+        return duration.getSeconds() * 1000 < expiration;
+    }
+
+    private void resetPasswordValidation(ResetPasswordWrapper wrapper) {
+        if (StringUtils.isAnyEmpty(wrapper.getVerificationCode(), wrapper.getNewPassword(), wrapper.getConfirmPassword())) {
+            throw new RuntimeException("Error: Field can't be blank.");
+        }
+
+        if (!wrapper.getNewPassword().equals(wrapper.getConfirmPassword())) {
+            throw new RuntimeException("Error: Confirmed password do not match!");
+        }
+
+        Pattern specialCharPatten = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        Pattern digitCasePatten = Pattern.compile("[0-9 ]");
+
+        if (wrapper.getNewPassword().length() < 6 || wrapper.getNewPassword().length() > 20) {
+            throw new RuntimeException("Error: Password length must be 6-20 characters!");
+        }
+
+        if (!specialCharPatten.matcher(wrapper.getNewPassword()).find()) {
+            throw new RuntimeException("Error: Password must have at least one special character!");
+        }
+
+        if (!digitCasePatten.matcher(wrapper.getNewPassword()).find()) {
+            throw new RuntimeException("Error: Password must have at least one digit character!");
+        }
     }
 }
