5903987e84ce176ed8d9c0d475c38dcd5959e313
akbarb24 <akbar.binaji@gmail.com>
2020-05-04 15:12:12 +0800

    refactor custom exception handler for auth

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/controller/AuthController.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/controller/AuthController.java
@@ -1,99 +1,66 @@
 package com.mitrais.chipper.temankondangan.backendapps.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
+import com.mitrais.chipper.temankondangan.backendapps.common.CommonResource;
+import com.mitrais.chipper.temankondangan.backendapps.common.response.ResponseBody;
+import com.mitrais.chipper.temankondangan.backendapps.model.json.LoginWrapper;
+import com.mitrais.chipper.temankondangan.backendapps.model.json.RegisterUserWrapper;
+import com.mitrais.chipper.temankondangan.backendapps.security.TokenProvider;
+import com.mitrais.chipper.temankondangan.backendapps.service.AuthService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.*;
 
-import com.mitrais.chipper.temankondangan.backendapps.common.CommonResource;
-import com.mitrais.chipper.temankondangan.backendapps.common.response.ResponseBody;
-import com.mitrais.chipper.temankondangan.backendapps.model.User;
-import com.mitrais.chipper.temankondangan.backendapps.model.json.LoginWrapper;
-import com.mitrais.chipper.temankondangan.backendapps.model.json.RegisterUserWrapper;
-import com.mitrais.chipper.temankondangan.backendapps.security.TokenProvider;
-import com.mitrais.chipper.temankondangan.backendapps.service.AuthService;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
+import javax.servlet.http.HttpServletRequest;
 
 @Api(value = "Auth")
 @RestController
 @RequestMapping("/auth")
 public class AuthController extends CommonResource {
 
-	@Autowired
-	AuthenticationManager authenticationManager;
+    @Autowired
+    AuthenticationManager authenticationManager;
 
-	@Autowired
-	AuthService authService;
+    @Autowired
+    AuthService authService;
 
-	@Autowired
-	TokenProvider tokenProvider;
+    @Autowired
+    TokenProvider tokenProvider;
 
-	@PostMapping("/login")
-	@ResponseStatus(HttpStatus.OK)
-	public ResponseEntity<ResponseBody> login(@RequestBody LoginWrapper data, HttpServletRequest request) {
-		boolean result = authService.login(data.getEmail(), data.getPassword());
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<ResponseBody> login(@RequestBody LoginWrapper data, HttpServletRequest request) {
+        authService.login(data.getEmail(), data.getPassword());
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(data.getEmail(), data.getPassword()));
+        String jwt = tokenProvider.createToken(authentication);
 
-		if (result) {
-			Authentication authentication = authenticationManager
-					.authenticate(new UsernamePasswordAuthenticationToken(data.getEmail(), data.getPassword()));
-			String jwt = tokenProvider.createToken(authentication);
-			return ResponseEntity.ok(getResponseBody(HttpStatus.OK.value(), jwt, null));
+        return ResponseEntity.ok(getResponseBody(HttpStatus.OK.value(), jwt, null));
+    }
 
-		} else {
-			return new ResponseEntity<>(getResponseBody(HttpStatus.UNAUTHORIZED, null, null, request.getRequestURI()),
-					HttpStatus.UNAUTHORIZED);
-		}
-	}
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<ResponseBody> register(@RequestBody RegisterUserWrapper register,
+                                                 HttpServletRequest request) {
+        authService.save(register);
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(register.getEmail(), register.getPassword()));
+        String jwt = tokenProvider.createToken(authentication);
 
-	@PostMapping("/register")
-	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<ResponseBody> register(@RequestBody RegisterUserWrapper register,
-			HttpServletRequest request) {
-		try {
-			User user = authService.save(register);
-			if (user.getUserId() != null) {
-				Authentication authentication = authenticationManager.authenticate(
-						new UsernamePasswordAuthenticationToken(register.getEmail(), register.getPassword()));
-				String jwt = tokenProvider.createToken(authentication);
-				return new ResponseEntity<>(getResponseBody(HttpStatus.CREATED.value(), jwt, null), HttpStatus.CREATED);
+        return new ResponseEntity<>(getResponseBody(HttpStatus.CREATED.value(), jwt, null), HttpStatus.CREATED);
+    }
 
-			} else {
-				return new ResponseEntity<>(
-						getResponseBody(HttpStatus.UNPROCESSABLE_ENTITY, null, null, request.getRequestURI()),
-						HttpStatus.UNPROCESSABLE_ENTITY);
-			}
+    @ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class, example = "Bearer <access_token>")
+    @GetMapping("/logout")
+    public ResponseEntity<ResponseBody> logout(HttpServletRequest request) {
+        String token = getToken(request.getHeader("Authorization"));
+        authService.logout(tokenProvider.getUserIdFromToken(token));
 
-		} catch (ResponseStatusException e) {
-			return new ResponseEntity<>(getResponseBody(e.getStatus(), null, e.getReason(), request.getRequestURI()),
-					e.getStatus());
-		}
-
-	}
-
-	@ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class, example = "Bearer <access_token>")
-	@GetMapping("/logout")
-	public ResponseEntity<ResponseBody> logout(HttpServletRequest request) {
-		String token = getToken(request.getHeader("Authorization"));
-		boolean result = authService.logout(tokenProvider.getUserIdFromToken(token));
-		if (result) {
-			return ResponseEntity.ok(getResponseBody(HttpStatus.OK.value(), null, null));
-		} else {
-			return new ResponseEntity<>(
-					getResponseBody(HttpStatus.UNPROCESSABLE_ENTITY, null, null, request.getRequestURI()),
-					HttpStatus.UNPROCESSABLE_ENTITY);
-		}
-	}
+        return ResponseEntity.ok(getResponseBody(HttpStatus.OK.value(), null, null));
+    }
 }
