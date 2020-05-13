ac5c1924291bd57dd42b31002a78468bf9cce5b0
akbarb24 <akbar.binaji@gmail.com>
2020-05-13 13:32:14 +0800

    add findByEventId method

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/repository/ApplicantRepository.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/repository/ApplicantRepository.java
@@ -2,8 +2,14 @@ package com.mitrais.chipper.temankondangan.backendapps.repository;
 
 import com.mitrais.chipper.temankondangan.backendapps.model.Applicant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ApplicantRepository extends JpaRepository<Applicant, Long> {
+    @Query("SELECT a from Applicant a WHERE a.event.eventId = :eventId")
+    List<Applicant> findByEventId(@Param("eventId")Long eventId);
 }
