9a17a83deb52c37303bd7ebdf0decc37b51dbaa9
Doni Kuncoro Rizky <donikrizky@gmail.com>
2020-05-20 15:01:50 +0700

    add fixing for sonarqube and remove add image in register

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/service/impl/EventServiceImpl.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/service/impl/EventServiceImpl.java
@@ -20,7 +20,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mitrais.chipper.temankondangan.backendapps.exception.BadRequestException;
 import com.mitrais.chipper.temankondangan.backendapps.exception.ResourceNotFoundException;
