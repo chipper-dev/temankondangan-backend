3d379d7da2a2f0d96c6bd65d3d31850fd754f64c
akbarb24 <akbar.binaji@gmail.com>
2020-05-27 09:30:29 +0800

    create EventControllerTest

---
+++ src/test/java/com/mitrais/chipper/temankondangan/backendapps/controller/EventControllerTest.java
@@ -0,0 +1,27 @@
+package com.mitrais.chipper.temankondangan.backendapps.controller;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class EventControllerTest {
+    private MockMvc mvc;
+    private WebApplicationContext context;
+
+    @BeforeAll
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+}
