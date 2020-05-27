e4ba78ac1aefef536afa27cf0279ffabf61accb1
akbarb24 <akbar.binaji@gmail.com>
2020-05-27 09:42:33 +0800

    setup auth in EventControllerTest

--- src/test/java/com/mitrais/chipper/temankondangan/backendapps/controller/EventControllerTest.java
+++ src/test/java/com/mitrais/chipper/temankondangan/backendapps/controller/EventControllerTest.java
@@ -1,12 +1,26 @@
 package com.mitrais.chipper.temankondangan.backendapps.controller;
 
+import com.mitrais.chipper.temankondangan.backendapps.model.User;
+import com.mitrais.chipper.temankondangan.backendapps.repository.UserRepository;
+import com.mitrais.chipper.temankondangan.backendapps.security.TokenProvider;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Optional;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
@@ -16,6 +30,25 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 public class EventControllerTest {
     private MockMvc mvc;
     private WebApplicationContext context;
+    private AuthenticationManager authenticationManager;
+    private TokenProvider tokenProvider;
+    private PasswordEncoder passwordEncoder;
+
+    private String token;
+    private User user;
+    private final String email = "test@mail.com";
+    private final String password = "test123!";
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Autowired
+    public EventControllerTest(WebApplicationContext context, AuthenticationManager authenticationManager, TokenProvider tokenProvider, PasswordEncoder passwordEncoder) {
+        this.context = context;
+        this.authenticationManager = authenticationManager;
+        this.tokenProvider = tokenProvider;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @BeforeAll
     public void setup() {
@@ -23,5 +56,21 @@ public class EventControllerTest {
                 .webAppContextSetup(context)
                 .apply(springSecurity())
                 .build();
+    }
+
+    @BeforeEach
+    public void initTest() {
+        user = User.builder().userId(1L).email(email).passwordHashed(passwordEncoder.encode(password)).build();
+
+        Mockito.when(userRepository.findByEmail(Mockito.anyString())).thenReturn(Optional.of(user));
+        Mockito.when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+        token = tokenProvider.createToken(authentication);
+    }
+
+    @Test
+    public void methodTest() {
+        System.out.println(token);
     }
 }
