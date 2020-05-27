f25b49fdcb2210f56de4955ad5ab0b84d8340a56
Doni Kuncoro Rizky <donikrizky@gmail.com>
2020-05-27 09:09:13 +0700

    Add status on find all event

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/repository/EventRepository.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/repository/EventRepository.java
@@ -23,10 +23,11 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 	Optional<List<Event>> findByUserId(@Param("userId") Long userId);
 
 	@Query("SELECT new com.mitrais.chipper.temankondangan.backendapps.model.json.EventFindAllListDBResponseWrapper"
-			+ "(e.eventId, p.profileId, p.fullName, e.createdBy,"
-			+ "e.title, e.city , e.startDateTime, e.finishDateTime,"
-			+ "e.minimumAge, e.maximumAge, p.gender, e.companionGender) from Event e "
+			+ "(e.eventId, p.profileId, p.fullName, e.createdBy, "
+			+ "e.title, e.city , e.startDateTime, e.finishDateTime, "
+			+ "e.minimumAge, e.maximumAge, p.gender, e.companionGender, a.status) from Event e "
 			+ "JOIN User u ON e.user.userId = u.userId " + "JOIN Profile p ON u.userId = p.user.userId "
+			+ "LEFT JOIN Applicant a ON a.applicantUser.userId = u.userId "
 			+ "WHERE ((e.minimumAge <= :age AND e.maximumAge >= :age "
 			+ "AND e.companionGender in :companionGender) "
 			+ "OR (u.userId = :userId)) "
