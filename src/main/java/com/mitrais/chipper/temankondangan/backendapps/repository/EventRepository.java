6f163f70167445e9c66d06bd054277b94a9cf0af
akbarb24 <akbar.binaji@gmail.com>
2020-05-28 10:36:22 +0800

    add query to find applied event list

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/repository/EventRepository.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/repository/EventRepository.java
@@ -5,6 +5,8 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+import com.mitrais.chipper.temankondangan.backendapps.model.en.DataState;
+import io.swagger.models.auth.In;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -19,20 +21,28 @@ import com.mitrais.chipper.temankondangan.backendapps.model.json.EventFindAllLis
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 
-	@Query("SELECT a from Event a WHERE a.user.userId = :userId")
-	Optional<List<Event>> findByUserId(@Param("userId") Long userId);
+    @Query("SELECT a from Event a WHERE a.user.userId = :userId")
+    Optional<List<Event>> findByUserId(@Param("userId") Long userId);
 
-	@Query("SELECT new com.mitrais.chipper.temankondangan.backendapps.model.json.EventFindAllListDBResponseWrapper"
-			+ "(e.eventId, p.profileId, p.fullName, e.createdBy,"
-			+ "e.title, e.city , e.startDateTime, e.finishDateTime,"
-			+ "e.minimumAge, e.maximumAge, p.gender, e.companionGender) from Event e "
-			+ "JOIN User u ON e.user.userId = u.userId " + "JOIN Profile p ON u.userId = p.user.userId "
-			+ "WHERE ((e.minimumAge <= :age AND e.maximumAge >= :age "
-			+ "AND e.companionGender in :companionGender) "
-			+ "OR (u.userId = :userId)) "
-			+ "AND e.startDateTime > :now")
-	Page<EventFindAllListDBResponseWrapper> findAllByRelevantInfo(@Param("age") Integer age,
-			@Param("companionGender") Collection<Gender> companionGender,@Param("userId") Long userId, @Param("now") LocalDateTime now,
-			Pageable paging);
+    @Query("SELECT new com.mitrais.chipper.temankondangan.backendapps.model.json.EventFindAllListDBResponseWrapper"
+            + "(e.eventId, p.profileId, p.fullName, e.createdBy,"
+            + "e.title, e.city , e.startDateTime, e.finishDateTime,"
+            + "e.minimumAge, e.maximumAge, p.gender, e.companionGender) from Event e "
+            + "JOIN User u ON e.user.userId = u.userId " + "JOIN Profile p ON u.userId = p.user.userId "
+            + "WHERE ((e.minimumAge <= :age AND e.maximumAge >= :age "
+            + "AND e.companionGender in :companionGender) "
+            + "OR (u.userId = :userId)) "
+            + "AND e.startDateTime > :now")
+    Page<EventFindAllListDBResponseWrapper> findAllByRelevantInfo(@Param("age") Integer age,
+                                                                  @Param("companionGender") Collection<Gender> companionGender, @Param("userId") Long userId, @Param("now") LocalDateTime now,
+                                                                  Pageable paging);
+
+    @Query("SELECT e FROM Event e " +
+            "JOIN Applicant a ON a.event.eventId = e.eventId " +
+            "WHERE a.applicantUser.userId = :userId " +
+            "AND a.dataState = 'ACTIVE' " +
+            "AND e.dataState = :dataStateEvent " +
+            "AND ((e.startDateTime >= :now AND :current = 1) OR (e.startDateTime < :now AND :current = 0))")
+    List<Event> findAppliedEvent(@Param("userId") Long userId, @Param("dataStateEvent") DataState dataState, @Param("now") LocalDateTime now, @Param("current") Integer current);
 
 }
