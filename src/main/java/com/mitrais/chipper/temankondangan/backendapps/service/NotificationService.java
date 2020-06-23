e02aaa6f251c51aa0b61c0867bca546e03dbb23b
Yuliawan Rizka S <yuliawanrizka.syafaat@mitrais.com>
2020-06-23 08:35:03 +0700

    add function to send multiple notification

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/service/NotificationService.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/service/NotificationService.java
@@ -1,9 +1,12 @@
 package com.mitrais.chipper.temankondangan.backendapps.service;
 
 import javax.validation.constraints.NotNull;
+import java.util.List;
 import java.util.Map;
 
 public interface NotificationService {
 
     void send(String title, String body, String messagingToken, @NotNull Map<String, String> data);
+
+    void sendMultiple(String title, String body, List<String> messagingTokens, @NotNull Map<String, String> data);
 }
--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/service/impl/NotificationServiceImpl.java
@@ -1,27 +1,16 @@
 package com.mitrais.chipper.temankondangan.backendapps.service.impl;
 
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
+import com.google.firebase.messaging.*;
 import com.mitrais.chipper.temankondangan.backendapps.exception.BadRequestException;
-import com.mitrais.chipper.temankondangan.backendapps.repository.UserRepository;
 import com.mitrais.chipper.temankondangan.backendapps.service.NotificationService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
-
-    private UserRepository userRepository;
-
-    @Autowired
-    public NotificationServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @Override
     public void send(String title, String body, String messagingToken, @NotNull Map<String, String> data) {
@@ -39,6 +28,28 @@ public class NotificationServiceImpl implements NotificationService {
                     .build();
 
             FirebaseMessaging.getInstance().send(message);
+
+        } catch (FirebaseMessagingException e) {
+            throw new BadRequestException("Error: " + e);
+        }
+    }
+
+    @Override
+    public void sendMultiple(String title, String body, List<String> messagingTokens, @NotNull Map<String, String> data) {
+        try{
+
+            Notification notification = Notification.builder()
+                    .setTitle(title)
+                    .setBody(body)
+                    .build();
+
+            MulticastMessage message = MulticastMessage.builder()
+                    .putAllData(data)
+                    .setNotification(notification)
+                    .addAllTokens(messagingTokens)
+                    .build();
+
+            FirebaseMessaging.getInstance().sendMulticast(message);
 
         } catch (FirebaseMessagingException e) {
             throw new BadRequestException("Error: " + e);
