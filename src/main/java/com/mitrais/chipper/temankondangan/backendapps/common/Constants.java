2bf9429e88a9ace1181fc072522c443dd3c0ce90
akbarb24 <akbar.binaji@gmail.com>
2020-06-25 11:28:46 +0800

    add rating data key

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/common/Constants.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/common/Constants.java
@@ -2,4 +2,9 @@ package com.mitrais.chipper.temankondangan.backendapps.common;
 
 public class Constants {
     public static final String DEFAULT_IMAGE = "image/defaultprofile.jpg";
+
+    public static class RatingDataKey {
+        public static final String AVG = "average";
+        public static final String TOT = "total";
+    }
 }
