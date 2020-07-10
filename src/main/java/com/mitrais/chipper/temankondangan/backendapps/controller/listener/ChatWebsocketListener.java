f3c63327a9f50e5a4afbb5fb20e98e52c47a3379
Yuliawan Rizka S <yuliawanrizka.syafaat@mitrais.com>
2020-07-10 09:32:58 +0700

    create chat feature

---
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/controller/listener/ChatWebsocketListener.java
@@ -0,0 +1,40 @@
+package com.mitrais.chipper.temankondangan.backendapps.controller.listener;
+
+import com.mitrais.chipper.temankondangan.backendapps.model.ChatMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+public class ChatWebsocketListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(ChatWebsocketListener.class);
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        logger.info("Received a new web socket connection");
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if(username != null) {
+            logger.info("User Disconnected : " + username);
+
+            ChatMessage chatMessage = new ChatMessage();
+            chatMessage.setType(ChatMessage.MessageType.LEAVE);
+            chatMessage.setSender(username);
+
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+}
