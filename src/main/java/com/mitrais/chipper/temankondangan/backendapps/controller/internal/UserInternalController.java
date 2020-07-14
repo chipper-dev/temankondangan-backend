fc07d161c5fc70ae7b0cc6cff67c38e9cbccd799
akbarb24 <akbar.binaji@gmail.com>
2020-07-14 16:21:14 +0800

    update url getmapping

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/controller/internal/UserInternalController.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/controller/internal/UserInternalController.java
@@ -3,17 +3,19 @@ package com.mitrais.chipper.temankondangan.backendapps.controller.internal;
 import com.mitrais.chipper.temankondangan.backendapps.model.User;
 import com.mitrais.chipper.temankondangan.backendapps.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user/{id}")
+@RequestMapping("/api/users")
 public class UserInternalController {
 
     @Autowired
     UserService userService;
 
+    @GetMapping("/{id}")
     public User getUser(@PathVariable("id") Long id) {
         return userService.findById(id);
     }
