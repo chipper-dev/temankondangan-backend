75b4e73819c84343ae02e7e7de0c22dec79fc6d5
Arif Setyo <arifsetyo.budi@mitrais.com>
2020-07-22 08:08:20 +0700

    Revert eureka thing. Update chatroom

--- src/main/java/com/mitrais/chipper/temankondangan/backendapps/BackendAppsApplication.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backendapps/BackendAppsApplication.java
@@ -10,10 +10,6 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-/*import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;*/
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
